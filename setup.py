d5797cd1e1c3cf93bdc67908785fd12a4e5351d9
tomg404 <32769321+tomg404@users.noreply.github.com>
2019-06-04 18:25:03 +0200

    Changed version number

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pushnotifier-tomg404",
-    version="0.0.1",
+    name="pushnotifier",
+    version="1.0.0",
     author="Tom Gaimann",
     author_email="tom.gaimann@outlook.com",
-    description="A python package for an easy use of https://pushnotifier.de/",
+    description="A python package for an easy use of the service from https://pushnotifier.de/",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/tomg404/pushnotifier-python",
