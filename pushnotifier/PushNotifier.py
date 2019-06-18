fd08e8b4e1a135482f3b744d48eaf34b51c5f093
tomg404 <32769321+tomg404@users.noreply.github.com>
2019-06-19 00:52:55 +0200

    added docstrings(); fixed send_image();

--- pushnotifier/PushNotifier.py
+++ pushnotifier/PushNotifier.py
@@ -1,13 +1,23 @@
 import requests
 import json
 import base64
-import random
+import uuid
 import string
 from pushnotifier.exceptions import *
 
 
 class PushNotifier:
+
     def __init__(self, username, password, package_name, api_key):
+        """
+        Initialize a new PushNotifier object
+
+        Args:
+            username (str): your username for https://pushnotifier.de
+            password (str): your password for https://pushnotifier.de
+            package_name (str): the package you want to send the messages to
+            api_key (str): your api key (https://pushnotifier.de/account/api)
+        """
         self.base_url = 'https://api.pushnotifier.de/v2'
         self.login_url = self.base_url + '/user/login'
         self.devices_url = self.base_url + '/devices'
@@ -20,20 +30,29 @@ class PushNotifier:
         self.app_token = self.__get_app_token(password)
         self.headers = {'X-AppToken': self.app_token}
 
-    # checks if username, password, package_name and api_key are valid
-    # and returns some infos
     def login(self, password):
+        """
+        Used to verify everything is working fine
+
+        Args:
+            password (str): your password for https://pushnotifier.de
+
+        Returns:
+            dict: basic information about your account
+        """
+
         login_data = {
-        'username': self.username,
-        'password': password
+            'username': self.username,
+            'password': password
         }
-        r = requests.post(self.login_url, json=login_data, auth=(self.package_name, self.api_key), headers=self.headers)
+        r = requests.post(self.login_url, json=login_data, auth=(
+            self.package_name, self.api_key), headers=self.headers)
         return r.json()
 
     def __get_app_token(self, password):
         login_data = {
-        'username': self.username,
-        'password': password
+            'username': self.username,
+            'password': password
         }
         r = requests.post(self.login_url, data=login_data, auth=(self.package_name, self.api_key))
 
@@ -47,41 +66,68 @@ class PushNotifier:
         app_token = json.loads(r.text)['app_token']
         return app_token
 
-
-    # calls https://api.pushnotifier.de/v2/user/refresh to refresh the app token
     def refresh_token(self):
-        r = requests.get(self.refresh_url, auth=(self.package_name, self.api_key), headers=self.headers)
+        """
+        Used to refresh your app token
+
+        Returns:
+            string: new app token
+        """
+        r = requests.get(self.refresh_url, auth=(
+            self.package_name, self.api_key), headers=self.headers)
         new_token = r.json()['app_token']
         self.app_token = new_token
         return new_token
 
-    # returns an array of the 'id's of every device
     def get_all_devices(self):
-        r = requests.get(self.devices_url, auth=(self.package_name, self.api_key), headers=self.headers)
+        """
+        Get all devices linked with your account
+
+        Returns:
+            list: list with all devices linked with your account
+
+        """
+        r = requests.get(self.devices_url, auth=(
+            self.package_name, self.api_key), headers=self.headers)
         devices = r.json()
         devices_array = []
         for index, _ in enumerate(devices):
             devices_array.append(devices[index]['id'])
         return devices_array
 
-    # sends text to all devices specified.
-    # if no device is specified it sends the message to every device.
-    # returns the response code of the api.
     def send_text(self, text, devices=None, silent=False):
+        """
+        Sends a text to all devices specified
+
+        Args:
+            text (str): the text you want to send
+            devices (list): a list of all devices you want to send the text to
+            silent (bool): if False the message triggers a sound
+
+        Returns:
+            int: error code or 200 if everything went fine
+
+        Raises:
+            MalformedRequestError: the request is malformed, i.e. missing content
+            DeviceNotFoundError: a device couldn\'t be found
+
+        """
+
         if devices == None:
             body = {
-            "devices": self.get_all_devices(),
-            "content": text,
-            "silent": silent
+                "devices": self.get_all_devices(),
+                "content": text,
+                "silent": silent
             }
         else:
             body = {
-            "devices": devices,
-            "content": text,
-            "silent": silent
+                "devices": devices,
+                "content": text,
+                "silent": silent
             }
 
-        r = requests.put(self.send_text_url, json=body, auth=(self.package_name, self.api_key), headers=self.headers)
+        r = requests.put(self.send_text_url, json=body, auth=(
+            self.package_name, self.api_key), headers=self.headers)
         if r.status_code == 200:
             return 200
         elif r.status_code == 400:
@@ -91,22 +137,38 @@ class PushNotifier:
             raise DeviceNotFoundError
             return 404
 
-    # basically same functionality as send_text
     def send_url(self, url, devices=None, silent=False):
+        """
+        Sends a url to all devices specified
+
+        Args:
+            url (str): the url you want to send
+            devices (list): a list of all devices you want to send the url to
+            silent (bool): if False the message triggers a sound
+
+        Returns:
+            int: error code or 200 if everything went fine
+
+        Raises:
+            MalformedRequestError: the request is malformed, i.e. missing content
+            DeviceNotFoundError: a device couldn\'t be found
+
+        """
         if devices == None:
             body = {
-            "devices": self.get_all_devices(),
-            "content": url,
-            "silent": silent
+                "devices": self.get_all_devices(),
+                "content": url,
+                "silent": silent
             }
         else:
             body = {
-            "devices": devices,
-            "content": url,
-            "silent": silent
+                "devices": devices,
+                "content": url,
+                "silent": silent
             }
 
-        r = requests.put(self.send_text_url, json=body, auth=(self.package_name, self.api_key), headers=self.headers)
+        r = requests.put(self.send_text_url, json=body, auth=(
+            self.package_name, self.api_key), headers=self.headers)
 
         if r.status_code == 200:
             return 200
@@ -117,24 +179,41 @@ class PushNotifier:
             raise DeviceNotFoundError
             return 404
 
-    # basically same functionality as send_text
     def send_notification(self, text, url, devices=None, silent=False):
+        """
+        Sends a notification (text + url) to all devices specified
+
+        Args:
+            text (str): the text you want to send
+            url (str): the url you want to send
+            devices (list): a list of all devices you want to send the notification to
+            silent (bool): if False the message triggers a sound
+
+        Returns:
+            int: error code or 200 if everything went fine
+
+        Raises:
+            MalformedRequestError: the request is malformed, i.e. missing content
+            DeviceNotFoundError: a device couldn\'t be found
+
+        """
         if devices == None:
             body = {
-            "devices": self.get_all_devices(),
-            "content": text,
-            "url": url,
-            "silent": silent
+                "devices": self.get_all_devices(),
+                "content": text,
+                "url": url,
+                "silent": silent
             }
         else:
             body = {
-            "devices": devices,
-            "content": text,
-            "url": url,
-            "silent": silent
+                "devices": devices,
+                "content": text,
+                "url": url,
+                "silent": silent
             }
 
-        r = requests.put(self.send_text_url, json=body, auth=(self.package_name, self.api_key), headers=self.headers)
+        r = requests.put(self.send_text_url, json=body, auth=(
+            self.package_name, self.api_key), headers=self.headers)
 
         if r.status_code == 200:
             return 200
@@ -145,31 +224,52 @@ class PushNotifier:
             raise DeviceNotFoundError
             return 404
 
-    # basically same functionality as send_text but you are passing an image to the function
     def send_image(self, image_path, devices=None, silent=False):
-        with open(image_path, "rb") as image_file:
+        """
+        Thanks to @Logxn (github/logxn) for this method
+        Sends an image to all devices specified
+
+        Args:
+            image_path (str): the path to the image you want to send
+            devices (list): a list of all devices you want to send the image to
+            silent (bool): if False the message triggers a sound
+
+        Returns:
+            int: error code or 200 if everything went fine
+
+        Raises:
+            MalformedRequestError: the request is malformed, i.e. missing content
+            DeviceNotFoundError: a device couldn\'t be found
+            PayloadTooLargeError: your image is too big (> 5 MB)
+            UnsupportedMediaTypeError: you passed an invalid file type or the device(s) you tried to send this image to can\'t receive images
+        """
+        with open(image_path, 'rb') as image_file:
             encoded_bytes = base64.b64encode(image_file.read())
 
         # encoded_image are base64 encoded bytes of the image_file bytes
         # since json can't handle raw bytes we need to decode them into a base64 string
-        encoded_image = encoded_bytes.decode('utf-8')
-        file_name = ''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase) for i in range(10))
+        encoded_image = encoded_bytes.decode()
+
+        # generate random file name
+        file_name = str(uuid.uuid4())
+
         if devices == None:
             body = {
-            "devices": self.get_all_devices(),
-            "content": encoded_image,
-            "filename": file_name,
-            "silent": silent
+                "devices": self.get_all_devices(),
+                "content": encoded_image,
+                "filename": file_name,
+                "silent": silent
             }
         else:
             body = {
-            "devices": devices,
-            "content": encoded_image,
-            "filename": file_name,
-            "silent": silent
+                "devices": devices,
+                "content": encoded_image,
+                "filename": file_name,
+                "silent": silent
             }
 
-        r = requests.put(self.send_image_url, json=body, auth=(self.package_name, self.api_key), headers=self.headers)
+        r = requests.put(self.send_image_url, json=body, auth=(
+            self.package_name, self.api_key), headers=self.headers)
 
         if r.status_code == 200:
             return 200
@@ -179,3 +279,13 @@ class PushNotifier:
         elif r.status_code == 404:
             raise DeviceNotFoundError
             return 404
+        elif r.status_code == 413:
+            raise PayloadTooLargeError
+            return 413
+        elif r.status_code == 415:
+            raise UnsupportedMediaTypeError
+            return 415
+        else:
+            raise UnknownError
+            print('Error code:', r.status_code, r.reason)
+            return -1
