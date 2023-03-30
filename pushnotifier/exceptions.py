d66db1e097fbf7dcc1f3296972b492202c4cb333
tomg404 <tom.gaimann@outlook.com>
2023-03-30 19:33:26 +0200

    turned exceptions into proper classes

--- pushnotifier/exceptions.py
+++ pushnotifier/exceptions.py
@@ -1,14 +1,44 @@
-MalformedRequestError = Exception("the request is malformed, i.e. missing content")
-DeviceNotFoundError = Exception("a device couldn't be found")
-UserNotFoundError = Exception("user couldn't be found (incorrect username/password)")
-IncorrectCredentialsError = Exception("credentials are incorrect")
-UnauthorizedError = Exception("package name or api key is incorrect")
-PayloadTooLargeError = Exception("your image is too big (> 5 MB)")
-UnsupportedMediaTypeError = Exception(
-    "you passed an invalid file type or the device(s) you tried to send this image "
-    "to can't receive images"
-)
+class MalformedRequestError(Exception):
+    def __init__(self, message="the request is malformed, i.e. missing content"):
+        super().__init__(message)
 
-UnknownError = Exception(
-    "an unknown error occurred! please contact the author of this module!"
-)
+
+class DeviceNotFoundError(Exception):
+    def __init__(self, message="a device couldn't be found"):
+        super().__init__(message)
+
+
+class UserNotFoundError(Exception):
+    def __init__(self, message="user couldn't be found (incorrect username/password)"):
+        super().__init__(message)
+
+
+class IncorrectCredentialsError(Exception):
+    def __init__(self, message="credentials are incorrect"):
+        super().__init__(message)
+
+
+class UnauthorizedError(Exception):
+    def __int__(self, message="package name or api key is incorrect"):
+        super().__init__(message)
+
+
+class PayloadTooLargeError(Exception):
+    def __init__(self, message="your image is too big (> 5 MB)"):
+        super().__init__(message)
+
+
+class UnsupportedMediaTypeError(Exception):
+    def __init__(
+        self,
+        message="you passed an invalid file type or the device(s) you tried to send this image to can't receive images",
+    ):
+        super().__init__(message)
+
+
+class UnknownError(Exception):
+    def __init__(
+        self,
+        message="an unknown error occurred! please contact the author of this module!",
+    ):
+        super().__init__(message)
