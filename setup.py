d03ea8b47526d2047ad1ebe6f531048d93de4ce9
tomg404 <tom.gaimann@outlook.com>
2023-03-30 20:11:57 +0200

    small improvements

--- setup.py
+++ setup.py
@@ -1,22 +1,24 @@
 import setuptools
 import pushnotifier
 
-with open('README.md', 'r', encoding='utf-8') as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name=pushnotifier.__name__,
     version=pushnotifier.__version__,
     author="Tom Gaimann",
-    author_email="tom.gaimann@outlook.com",
-    description="A python package for an easy use of the service from https://pushnotifier.de/",
+    author_email="tom.gaimann+pushnotifier@outlook.com",
+    description="A python package for easy interaction with https://pushnotifier.de/",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/tomg404/pushnotifier-python",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ]
+    ],
 )
